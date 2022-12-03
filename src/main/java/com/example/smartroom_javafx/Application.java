4fdabce831dd6668a8b35d680fb445a7e2cc9a59
Gabriel Mohamed-Hofmayr <gabhof1@gmx.at>
2022-12-03 11:17:06 +0100

    Erweiterung Startpage

--- src/main/java/com/example/smartroom_javafx/Application.java
+++ src/main/java/com/example/smartroom_javafx/Application.java
@@ -10,7 +10,7 @@ public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("DigitalTwinSceneBuilder.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 900, 700);
+        Scene scene = new Scene(fxmlLoader.load(), 700, 450);
         stage.setTitle("Smart Room - Digital Twin");
         stage.setScene(scene);
         stage.show();
